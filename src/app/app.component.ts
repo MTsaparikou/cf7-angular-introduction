35064a9a1173352cabe2aeef9bb8ab928efca7e4
MTsaparikou <magda.tsp@hotmail.com>
2025-04-21 14:13:00 +0300

    New component person table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonTableComponent } from './componets/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-introduction';
+  name = 'Magda';
+
+  person = {
+    givenName: 'Magda',
+    surName: 'Tsaparikou',
+    age:25,
+    email: 'mtsaparikou@aueb.gr'
+  }
 }
